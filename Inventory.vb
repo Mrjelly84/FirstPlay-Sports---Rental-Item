9d1d5fb984d354b0425e9baf556887cc8bdfd299
william dew <williamrdew@gmail.com>
2025-06-14 12:00:12 +0100

    making classes

---
+++ Inventory.vb
@@ -0,0 +1,30 @@
+﻿Public Class Inventory
+    Private InventoryList As New Dictionary(Of String, Item)
+    Public ReadOnly Property Items As Dictionary(Of String, Item).ValueCollection
+        Get
+            Return InventoryList.Values
+        End Get
+    End Property
+
+    Public Sub AddItem(item As Item)
+        InventoryList.Add(item.IDNumber, item)
+    End Sub
+
+    Public Sub RemoveItem(Id As String)
+
+        InventoryList.Remove(Id)
+
+    End Sub
+
+    Public Function GetItem(Id As String) As Item
+        Try
+            Return InventoryList(Id)
+        Catch ex As Exception
+
+            Return Nothing
+        End Try
+
+    End Function
+
+
+End Class
---
+++ Item.vb
@@ -0,0 +1,10 @@
+﻿Public Class Item
+
+    Public Property Quantity As Integer
+    Public Property IDNumber As String
+    Public Property Description As String
+    Public Property DailyPrice As Single
+    Public Property WeeklyPrice As Single
+    Public Property MonthlyPrice As Single
+
+End Class
